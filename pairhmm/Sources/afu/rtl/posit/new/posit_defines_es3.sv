97c7b0447fe5d37fbb43c6d2bdf163729357499e
lvandam <laurens@vand.am>
2018-05-10 12:00:25 +0200

    WIP es3 mult

---
+++ pairhmm/Sources/afu/rtl/posit/new/posit_defines_es3.sv
@@ -0,0 +1,39 @@
+// Laurens van Dam
+// Delft University of Technology
+// May 2018
+
+package posit_defines;
+
+parameter NBITS = 32;
+parameter ES = 3;
+parameter FBITS = NBITS - 3 - ES; // Size of fraction bits
+parameter FHBITS = FBITS + 1; // Size of fraction + hidden bit
+parameter MBITS = 2 *  FHBITS; // Size of multiplier output
+parameter ABITS = FBITS + 4; // Size of addend
+
+typedef struct {
+    logic sign;
+    logic signed [8:0] scale;
+    logic [ES-1:0] exponent;
+    logic [FBITS-1:0] fraction;
+    logic inf;
+    logic zero;
+} value;
+
+typedef struct {
+    logic sign;
+    logic signed [9:0] scale;
+    logic [MBITS-1:0] fraction;
+    logic inf;
+    logic zero;
+} value_product;
+
+typedef struct {
+    logic sign;
+    logic signed [8:0] scale;
+    logic [ABITS-1:0] fraction;
+    logic inf;
+    logic zero;
+} value_sum;
+
+endpackage : posit_defines
