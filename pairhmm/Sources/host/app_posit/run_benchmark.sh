b4da2ad75ced918c3f28e020c36aa1d3fc631df4
Laurens van Dam <laurens@vand.am>
2018-08-23 14:51:23 +0200

    Update benchmark script

--- pairhmm/Sources/host/app_posit/run_benchmark.sh
+++ pairhmm/Sources/host/app_posit/run_benchmark.sh
@@ -5,36 +5,36 @@ for p in 16 32
 do
     for i in 5 10 15
     do
-        make host-run-posit $p 8 8 $i
-        make host-run-posit $p 16 16 $i
-        make host-run-posit $p 24 24 $i
-        make host-run-posit $p 32 32 $i
-        make host-run-posit $p 40 40 $i
+        $DIR/build/pairhmm_host $p 8 8 $i
+        $DIR/build/pairhmm_host $p 16 16 $i
+        $DIR/build/pairhmm_host $p 24 24 $i
+        $DIR/build/pairhmm_host $p 32 32 $i
+        $DIR/build/pairhmm_host $p 40 40 $i
 
-        make host-run-posit $p 8 16 $i
+        $DIR/build/pairhmm_host $p 8 16 $i
 
-        make host-run-posit $p 8 24 $i
-        make host-run-posit $p 16 24 $i
+        $DIR/build/pairhmm_host $p 8 24 $i
+        $DIR/build/pairhmm_host $p 16 24 $i
 
-        make host-run-posit $p 8 32 $i
-        make host-run-posit $p 16 32 $i
-        make host-run-posit $p 24 32 $i
+        $DIR/build/pairhmm_host $p 8 32 $i
+        $DIR/build/pairhmm_host $p 16 32 $i
+        $DIR/build/pairhmm_host $p 24 32 $i
 
-        make host-run-posit $p 8 40 $i
-        make host-run-posit $p 16 40 $i
-        make host-run-posit $p 24 40 $i
-        make host-run-posit $p 32 40 $i
+        $DIR/build/pairhmm_host $p 8 40 $i
+        $DIR/build/pairhmm_host $p 16 40 $i
+        $DIR/build/pairhmm_host $p 24 40 $i
+        $DIR/build/pairhmm_host $p 32 40 $i
 
-        make host-run-posit $p 8 48 $i
-        make host-run-posit $p 16 48 $i
-        make host-run-posit $p 24 48 $i
-        make host-run-posit $p 32 48 $i
-        make host-run-posit $p 40 48 $i
+        $DIR/build/pairhmm_host $p 8 48 $i
+        $DIR/build/pairhmm_host $p 16 48 $i
+        $DIR/build/pairhmm_host $p 24 48 $i
+        $DIR/build/pairhmm_host $p 32 48 $i
+        $DIR/build/pairhmm_host $p 40 48 $i
 
-        make host-run-posit $p 8 56 $i
-        make host-run-posit $p 16 56 $i
-        make host-run-posit $p 24 56 $i
-        make host-run-posit $p 32 56 $i
-        make host-run-posit $p 40 56 $i
+        $DIR/build/pairhmm_host $p 8 56 $i
+        $DIR/build/pairhmm_host $p 16 56 $i
+        $DIR/build/pairhmm_host $p 24 56 $i
+        $DIR/build/pairhmm_host $p 32 56 $i
+        $DIR/build/pairhmm_host $p 40 56 $i
     done
 done
